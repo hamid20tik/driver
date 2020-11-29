926240303a81908a759d2db7438b182f42070e53
hamidlatop <karimnasab.shahrdari@gmail.com>
2020-11-30 00:36:01 +0330

    1- create driver app 2- create models driver 3- create super user 4- create views and urls 5- create idea for pages 6- create admin page for test db all ok

--- myblog/app1/admin.py
+++ myblog/app1/admin.py
@@ -1,8 +1,1 @@
 from django.contrib import admin
-from .models import car,person,safar,driver
-
-# Register your models here.
-admin.site.register(car)
-admin.site.register(person)
-admin.site.register(safar)
-admin.site.register(driver)
