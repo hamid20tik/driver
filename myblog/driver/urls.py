4952dd836c56848a113bdc1cd4d034dd0a19f899
hamidlatop <karimnasab.shahrdari@gmail.com>
2020-12-01 21:45:35 +0330

    block content 1 - yek page ra be onvan page mabna gharar midahim va bad ba estefade az {% block content %} {% endblock %} on ro ok mikonim va page badi ro tavasote {% extends 'index.html' %} kari mokonim ke ers bari kone az page asli 2*** dar ghesmate block content ers vari anjam nemishavad

--- myblog/driver/urls.py
+++ myblog/driver/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.login_page),
-    path('add-driver',views.add_driver),
-    path('enter-travel',views.enter_travel),
-    path('end-travel',views.end_travel),
-    path('home-page',views.home_page),
-    path('report-page',views.report_page),
+    path('add-driver/',views.add_driver),
+    path('enter-travel/',views.enter_travel),
+    path('end-travel/',views.end_travel),
+    path('home-page/',views.home_page ),
+    path('report-page/',views.report_page),
 ]
