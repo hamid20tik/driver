08ea6b46db7e3840391ddde514b3f4654199e683
HN <hamid20tik@gmail.com>
2020-12-02 19:36:59 +0330

    insert to db 1 - khandan etelaat az karbar va ferestadan be data base 2 - dar ghesmate view ha tavasote metod POST tavanestim etelaat ra be db vared konim 3 - baraye login kardan : 3-1 -     if request.method == 'POST':      اگر مقدار بازشکتی از اج تی ام ال برابر پست بود         user = auth.authenticate(username=request.POST['username'],password=request.POST['password'])         if user is not None:             auth.login(request, user)             return redirect('home_page')         else:             return render(request, 'login.html', {'error': 'نام کاربری یا رمز عبور اشتباه است'})     else:         return render(request,'login.html')  4 - code haye bala nemaesh gar hamechiz ast

--- myblog/driver/urls.py
+++ myblog/driver/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login_page),
-    path('add-driver/',views.add_driver),
-    path('enter-travel/',views.enter_travel),
-    path('end-travel/',views.end_travel),
-    path('home-page/',views.home_page ),
-    path('report-page/',views.report_page),
+    path('', views.login_page, name='login_page'),
+    path('logout',views.logout , name= 'logout'),
+    path('add-driver/', views.add_driver, name='add_driver'),
+    path('enter-travel/', views.enter_travel, name='enter_travel'),
+    path('end-travel/', views.end_travel, name='end_travel'),
+    path('home-page/', views.home_page, name='home_page'),
+    path('report-page/', views.report_page, name='report_page'),
+    path('add-person/', views.add_person, name='add_person'),
 ]
--- myblog/driver/views.py
+++ myblog/driver/views.py
@@ -1,23 +1,71 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from .models import driver,person,safar
+from django.contrib.auth.decorators import login_required
+from django.contrib import auth
 from django.http import HttpResponse
 from django.template import loader,context
 from .models import safar
 # Create your views here.
-
+@login_required
 def add_driver(request):
-    return render(request,'add_driver.html')
-
+    if request.method == "POST":
+        if request.POST['name'] and request.POST['fname'] and request.POST['codemeli'] and request.POST['phone'] and request.POST['car_name'] and request.POST['car_model'] and request.POST['car_pelak']:
+            dri = driver()
+            dri.name= request.POST['name']
+            dri.family = request.POST['fname']
+            dri.codemeli = request.POST['codemeli']
+            dri.phone = request.POST['phone']
+            dri.car_name = request.POST['car_name']
+            dri.car_model = request.POST['car_model']
+            dri.car_pelak = request.POST['car_pelak']
+            dri.save()
+            return redirect('home_page')
+        else:
+            return render(request,'add_driver.html',{'error':'تمامی مقادیر را به درستی وارد کنید '})
+    else:
+        return render(request,'add_driver.html')
+@login_required
 def end_travel(request):
     return render(request,'end_travel.html')
-
+@login_required
 def enter_travel(request):
-    return render(request,'enter_travel.html')
-
+    return render(request, 'enter_travel.html')
+@login_required
 def home_page(request):
-    return render(request,'index.html')
+    return render(request, 'index.html')
 
 def login_page(request):
-    return render(request,'login.html')
+    if request.method == 'POST':
+        user = auth.authenticate(username=request.POST['username'],password=request.POST['password'])
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home_page')
+        else:
+            return render(request, 'login.html', {'error': 'نام کاربری یا رمز عبور اشتباه است'})
+    else:
+        return render(request,'login.html')
+@login_required
+def logout(requset):
+    if requset.method=='POST':
+        auth.logout(requset)
+        return redirect('login_page')
 
+@login_required
+def add_person(request):
+    if request.method == 'POST':
+        if request.POST['name'] and request.POST['fname'] and request.POST['codeperson']:
+            per = person()
+            per.name=request.POST['name']
+            per.family = request.POST['fname']
+            per.person_code = request.POST['codeperson']
+            per.save()
+            return redirect('home_page')
+        else:
+            return render(request,'add_person.html',{'error':'تمامی مقادیر را به درستی وارد کنید '})
+    else:
+        return render(request,'add_person.html')
+
+@login_required
 def report_page(request):
     return render(request,'report.html')
