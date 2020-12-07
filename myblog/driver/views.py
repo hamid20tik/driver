4b63e991fe1409358eda050c73b36a48f2780e3c
HN <hamid20tik@gmail.com>
2020-12-07 23:24:17 +0330

    create table safarbackup 1 - baraye inke gozareshat ra be tor koli dashte bashim 2- in table felan be sorate test mibashad 23:23

--- myblog/driver/views.py
+++ myblog/driver/views.py
@@ -72,7 +72,5 @@ def add_person(request):
 
 @login_required
 def report_page(request):
-    if request.method=="POST":
-
     obj = safar.objects.all()
     return render(request,'report.html', {'rep_safar':obj})
