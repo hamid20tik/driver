e9fc063aa58c24a5c9108e35e6360e0f86f70154
HN <hamid20tik@gmail.com>
2020-12-16 20:59:55 +0330

    bootstrap for forms.py 1 - dar forms.py tavasote code     maghsad = forms.CharField(widget=forms.TextInput(         attrs={'class': 'form-control',                }     ))     tavanestim class bootstrap ra be file bedahim 2 - mamoriat , mabda , maghsad ra ke dar models haye ma hastan inja miyarim va khodesh ok mishe va niyazi nist ke berim baz tarif konim === <<<maghsad = forms.CharField>>>

--- myblog/driver/forms.py
+++ myblog/driver/forms.py
@@ -2,6 +2,20 @@ from django import forms
 from .models import safar,driver,person
 
 class safarform(forms.ModelForm):
+    mabda = forms.CharField(widget=forms.TextInput(
+        attrs={'class':'form-control',
+        }
+    ))
+    mamoriat = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control',
+               }
+    ))
+    maghsad = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control',
+               }
+    ))
+
+
     class Meta:
         model = safar
-        fields = ['driver','person','mamoriat','mabda','maghsad','datetime_start']
+        fields = ['driver','person','mamoriat','mabda','maghsad',]
